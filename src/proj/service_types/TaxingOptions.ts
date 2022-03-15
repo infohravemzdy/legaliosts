c64669115da0241c42091635d56927734274172b
mzdyhrave <info@hravemzdy.org>
2022-03-15 09:01:55 +0100

    Release 0.22.3.

--- src/proj/service_types/TaxingOptions.ts
+++ src/proj/service_types/TaxingOptions.ts
@@ -1,19 +1,19 @@
-enum TaxDeclSignOption {
+export enum TaxDeclSignOption {
   DECL_TAX_NO_SIGNED = 0,
   DECL_TAX_DO_SIGNED = 1,
 }
 
-enum TaxNoneSignOption {
+export enum TaxNoneSignOption {
   NOSIGN_TAX_WITHHOLD = 0,
   NOSIGN_TAX_ADVANCES = 1,
 }
 
-enum TaxDeclBenfOption {
+export enum TaxDeclBenfOption {
   DECL_TAX_BENEF0 = 0,
   DECL_TAX_BENEF1 = 1,
 }
 
-enum TaxDeclDisabOption {
+export enum TaxDeclDisabOption {
   DECL_TAX_BENEF0 = 0,
   DECL_TAX_DISAB1 = 1,
   DECL_TAX_DISAB2 = 2,
